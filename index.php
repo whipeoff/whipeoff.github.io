4b5d6fa01eab4a9ebb45b8258598e7c2826b145d
Whipe <71684900+whipeoff@users.noreply.github.com>
2025-04-21 11:06:25 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=http://techno-world.free.nf">
+  </head>
+  <body>
+    <p>Redirecting to <a href="http://techno-world.free.nf">http://techno-world.free.nf</a></p>
+  </body>
+</html>
